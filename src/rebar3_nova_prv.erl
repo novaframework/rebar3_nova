e9689101a6c0f6c000a9253e4ed3d05cbb19a602
Daniel Widgren <daniel.widgren@gmail.com>
2020-09-15 21:54:04 +0200

    print out

--- src/rebar3_nova_prv.erl
+++ src/rebar3_nova_prv.erl
@@ -3,7 +3,7 @@
 -export([init/1, do/1, format_error/1]).
 
 -define(PROVIDER, help).
--define(DEPS, [app_discovery]).
+-define(DEPS, [default, lock]).
 
 %% ===================================================================
 %% Public API
