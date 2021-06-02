d654a765085900c493a90ff64a10de98b548c83a
Niclas Axelsson <niclas.axelsson@teliacompany.com>
2021-06-02 15:45:13 +0200

    Fix path

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -o install.sh https://raw.githubusercontent.com/novaframework/rebar3_nova/master/install.escript
+curl -o install.escript https://raw.githubusercontent.com/novaframework/rebar3_nova/master/install.escript
 chmod +x install.escript
 ./install.escript
 rm install.escript
