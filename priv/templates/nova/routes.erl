bb6257976e3342c8c71bb01a8e3f2834a052c455
Niclas Axelsson <niclas@burbas.se>
2021-12-10 16:58:04 +0100

    Update templates with the new router-layout

--- priv/templates/nova/routes.erl
+++
@@ -1,7 +0,0 @@
-#{prefix => "",
-  security => false,
-  routes => [
-            {"/", { {{name}}_main_controller, index}, #{methods => [get]}},
-            {"/assets/[...]", "assets"}
-           ]
-}.
