0f076058f9412817a646d5d0187aad4c34ad3848
Niclas Axelsson <niclas@burbas.se>
2023-12-22 22:36:37 +0100

    The application itself should be responsible for boss_db

--- priv/templates/nova/sup.erl
+++ priv/templates/nova/sup.erl
@@ -28,8 +28,21 @@ start_link() ->
 
 %% Child :: {Id,StartFunc,Restart,Shutdown,Type,Modules}
 init([]) ->
+    {{#use_bossdb}}
+    %% Start boss_db
+    Configuration = application:get_env(nova, database_config, #{}),
+    setup_bossdb(Configuration),
+    {{/use_bossdb}}
     {ok, { {one_for_all, 0, 1}, []} }.
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
+{{#use_bossdb}}
+setup_bossdb(#{}) ->
+    logger:warning(#{msg => "Could not start boss_db because of empty configuration"});
+setup_bossdb(Configuration) ->
+    logger:debug(#{msg => "Starting boss_db with config", config => Configuration}),
+    boss_db:start(Configuration),
+    boss_news:start().
+{{/use_bossdb}}
