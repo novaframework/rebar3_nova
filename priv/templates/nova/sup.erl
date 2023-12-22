44460079a8f2db83e4553e83743fa3538c7cd264
Niclas Axelsson <niclas@burbas.se>
2023-12-22 22:47:07 +0100

    Change config-keys and include a simple database config as default when use_bossdb is set

--- priv/templates/nova/sup.erl
+++ priv/templates/nova/sup.erl
@@ -30,7 +30,7 @@ start_link() ->
 init([]) ->
     {{#use_bossdb}}
     %% Start boss_db
-    Configuration = application:get_env(nova, database_config, #{}),
+    Configuration = application:get_env(nova, database_configuration, #{}),
     setup_bossdb(Configuration),
     {{/use_bossdb}}
     {ok, { {one_for_all, 0, 1}, []} }.
@@ -43,6 +43,6 @@ setup_bossdb(#{}) ->
     logger:warning(#{msg => "Could not start boss_db because of empty configuration"});
 setup_bossdb(Configuration) ->
     logger:debug(#{msg => "Starting boss_db with config", config => Configuration}),
-    boss_db:start(Configuration),
+    boss_db:start(maps:to_list(Configuration)),
     boss_news:start().
 {{/use_bossdb}}
