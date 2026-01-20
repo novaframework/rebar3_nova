9d5a812248f3c450654f0ee1ab1fc7008b345240
Richard Carlsson <carlsson.richard@gmail.com>
2026-01-20 11:09:20 +0100

    Format with erlfmt 1.7

--- priv/templates/nova/router.erl
+++ priv/templates/nova/router.erl
@@ -2,15 +2,18 @@
 -behaviour(nova_router).
 
 -export([
-         routes/1
-        ]).
+    routes/1
+]).
 
 %% The Environment-variable is defined in your sys.config in {nova, [{environment, Value}]}
 routes(_Environment) ->
-    [#{prefix => "",
-      security => false,
-      routes => [
-                 {"/", fun {{name}}_main_controller:index/1 , #{methods => [get]}},
-                 {"/heartbeat", fun(_) -> {status, 200} end, #{methods => [get]}}
-                ]
-      }].
+    [
+        #{
+            prefix => "",
+            security => false,
+            routes => [
+                {"/", fun {{name}}_main_controller:index/1, #{methods => [get]}},
+                {"/heartbeat", fun(_) -> {status, 200} end, #{methods => [get]}}
+            ]
+        }
+    ].
